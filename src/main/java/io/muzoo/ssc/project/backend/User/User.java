b462646ef4d1e0ed61e59a9435b9a047586365c7
Nate-81 <nathans8100@gmail.com>
2024-03-11 22:32:46 +0700

    basic user class, creates table in DB

---
+++ src/main/java/io/muzoo/ssc/project/backend/User/User.java
@@ -0,0 +1,26 @@
+package io.muzoo.ssc.project.backend.User;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(unique = true)
+    private String username;
+
+    private String password;
+
+    private String display_name;
+
+    private String role;
+}
