7a396cf4e64a38c20de05eb1f1b8a5d7d40da46b
Nate-81 <nathans8100@gmail.com>
2024-03-11 14:00:47 +0700

    rename package and group config files

--- src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
+++ src/main/java/io/muzoo/ssc/project/backend/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.securingweb;
+package io.muzoo.ssc.project.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
