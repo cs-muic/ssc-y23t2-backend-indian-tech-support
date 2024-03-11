7f06353181cd6f0aa0e4b0547a8c0b64beab4baa
Nate-81 <nathans8100@gmail.com>
2024-03-11 22:58:45 +0700

    feat: Works with DB

--- src/main/java/io/muzoo/ssc/project/backend/auth/OurUserDetailsService.java
+++ src/main/java/io/muzoo/ssc/project/backend/auth/OurUserDetailsService.java
@@ -1,28 +1,24 @@
-package io.muzoo.ssc.project.backend;
+package io.muzoo.ssc.project.backend.auth;
 
+import io.muzoo.ssc.project.backend.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OurUserDetailsService implements UserDetailsService {
 
-    private final PasswordEncoder passwordEncoder;
-
-    public OurUserDetailsService(@Lazy PasswordEncoder passwordEncoder) {
-        this.passwordEncoder = passwordEncoder;
-    }
+    @Autowired
+    private UserRepository userRepository;
 
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        if ("admin".equals(username)){
-            return User.withUsername(username).password(passwordEncoder.encode("123456"))
-                    .roles("USER")
-                    .build();
+        io.muzoo.ssc.project.backend.User.User user = userRepository.findByUsername(username);
+        if (user != null){
+            return User.withUsername(user.getUsername()).password(user.getPassword())
+                    .roles(user.getRole()).build();
         } else{
             throw new UsernameNotFoundException("User not found");
         }
