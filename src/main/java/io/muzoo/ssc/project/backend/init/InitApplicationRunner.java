ef5edf4bd1cc2340e68cf82efe2a3d8d0ceb0bf1
Nate-81 <nathans8100@gmail.com>
2024-03-11 22:58:21 +0700

    feat: initialises DB with admin

---
+++ src/main/java/io/muzoo/ssc/project/backend/init/InitApplicationRunner.java
@@ -0,0 +1,33 @@
+package io.muzoo.ssc.project.backend.init;
+
+import io.muzoo.ssc.project.backend.User.User;
+import io.muzoo.ssc.project.backend.User.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class InitApplicationRunner implements ApplicationRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        User admin = userRepository.findByUsername("admin");
+        if (admin == null){
+            User user = new User();
+            user.setUsername("admin");
+            user.setPassword(passwordEncoder.encode("admin"));
+            user.setDisplay_name("Admin");
+            user.setRole("ADMIN");
+            userRepository.save(user);
+        }
+
+    }
+}
