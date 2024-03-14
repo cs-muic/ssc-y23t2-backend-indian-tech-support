69fa511fe0fa888c5b1aa285b2319921c92fe024
Anish Khanijaur <anishkhanijaur089@gmail.com>
2024-03-14 16:48:34 +0700

    feat: Added transaction class

--- src/main/java/io/muzoo/ssc/project/backend/history/HistoryController.java
+++ src/main/java/io/muzoo/ssc/project/backend/history/HistoryController.java
@@ -1,5 +1,7 @@
-package io.muzoo.ssc.project.backend.whoami;
+package io.muzoo.ssc.project.backend.history;
 
+import io.muzoo.ssc.project.backend.Transaction.Transaction;
+import io.muzoo.ssc.project.backend.Transaction.TransactionRepository;
 import io.muzoo.ssc.project.backend.User.User;
 import io.muzoo.ssc.project.backend.User.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +13,19 @@ import org.springframework.web.bind.annotation.RestController;
  * A controller to retrieve current logged-in user.
  */
 @RestController
-public class WhoamiController {
+public class HistoryController {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private TransactionRepository transactionRepository;
+
     /**
      * Make sure that all API path begins with /api. This ends up being useful for when we do proxy
      */
-    @GetMapping("/api/whoami")
-    public WhoAmIDTO whoami() {
+    @GetMapping("/api/history")
+    public HistoryDTO history() {
         try {
             // The line below has the potential for a NullPointException due to nesting dot notation
             Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
@@ -28,18 +33,16 @@ public class WhoamiController {
                 // user is logged in
                 org.springframework.security.core.userdetails.User user = (org.springframework.security.core.userdetails.User) principal;
                 User u = userRepository.findByUsername(user.getUsername());
-                return WhoAmIDTO.builder()
+                return HistoryDTO.builder()
                         .loggedIn(true)
-                        .displayName(u.getDisplayName())
-                        .role(u.getRole())
-                        .username(u.getUsername())
+                        .transactions(transactionRepository.findAllByUserId(u.getId()))
                         .build();
             }
         } catch (Exception e) {
             // Ajarn just left this blank lmao
         }
         // user is not logged in
-        return WhoAmIDTO.builder()
+        return HistoryDTO.builder()
                 .loggedIn(false)
                 .build();
     }
--- src/main/java/io/muzoo/ssc/project/backend/history/HistoryDTO.java
+++ src/main/java/io/muzoo/ssc/project/backend/history/HistoryDTO.java
@@ -1,22 +1,19 @@
-package io.muzoo.ssc.project.backend.whoami;
+package io.muzoo.ssc.project.backend.history;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import io.muzoo.ssc.project.backend.Transaction.Transaction;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.List;
 
 @Getter
 @Setter
 @Builder
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-public class WhoAmIDTO {
+public class HistoryDTO {
+    private List<Transaction> transactions;
 
     private boolean loggedIn = false;
-
-    private String username;
-
-    private String role;
-
-    private String displayName;
-
 }
