eb0d579cb2233291bccc742c0bff2e5051a81a5d
Hir0Yat0 <xToppyThailandx@gmail.com>
2024-03-17 03:08:07 +0700

    refactor(transaction type): parsetype now call valueOf() instead

--- src/main/java/io/muzoo/ssc/project/backend/Transaction/Type.java
+++ src/main/java/io/muzoo/ssc/project/backend/Transaction/Type.java
@@ -16,15 +16,20 @@ public enum Type {
     @Getter
     private final String name;
 
-    public static Type parseType(String input) {
-        if (StringUtils.equals(input, "EXPENDITURE")) {
-            return EXPENDITURE;
-        } else if (StringUtils.equals(input, "INCOME")) {
-            return INCOME;
-        } else {
+    public static Type parseType(final String input) {
+
+        try {
+            final Type type = Type.valueOf(input);
+            return type;
+        }
+        catch (IllegalArgumentException | NullPointerException e){
+            // happens if no match or input is null
+            e.printStackTrace();
             return NONE;
         }
+
     }
+    
 }
 
 
