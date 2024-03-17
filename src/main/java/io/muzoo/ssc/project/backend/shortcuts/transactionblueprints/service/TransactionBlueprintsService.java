5d8ef1652bb16d4687a3861ca717390d69f2081a
Hir0Yat0 <xToppyThailandx@gmail.com>
2024-03-18 06:22:01 +0700

    feat(service): added TransactionBlueprintsService (not implemeted yet just add file and setups)

---
+++ src/main/java/io/muzoo/ssc/project/backend/shortcuts/transactionblueprints/service/TransactionBlueprintsService.java
@@ -0,0 +1,21 @@
+package io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.service;
+
+import org.springframework.stereotype.Service;
+
+import io.muzoo.ssc.project.backend.User.User;
+import io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.TransactionBlueprintsDTO;
+
+@Service
+public class TransactionBlueprintsService {
+
+    public TransactionBlueprintsDTO getTransactionBlueprints(User user) {
+        return null;
+    }
+
+    public TransactionBlueprintsDTO putTransactionBlueprints(User user) {
+        return null;
+    }
+    
+
+
+}
