084d6d34b3bb45f713a33cdf3ba6f769a53e9c51
Hir0Yat0 <xToppyThailandx@gmail.com>
2024-03-18 12:46:37 +0700

    fix: transaction blueprints dto now contain a list of transaction blueprints

--- src/main/java/io/muzoo/ssc/project/backend/shortcuts/transactionblueprints/TransactionBlueprintsDTO.java
+++ src/main/java/io/muzoo/ssc/project/backend/shortcuts/transactionblueprints/TransactionBlueprintsDTO.java
@@ -1,6 +1,6 @@
 package io.muzoo.ssc.project.backend.shortcuts.transactionblueprints;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 
@@ -14,13 +14,6 @@ import lombok.Setter;
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class TransactionBlueprintsDTO {
 
-    private long id;
-    private long userId;
-    private long tagId;
-    private long tagId2;
-    private String transactionType; // Assuming Type is an enum with values like INCOME, EXPENDITURE
-    private String shortcutType; // Assuming Type is an enum with values like FAVORITES, RECURRING
-    private String notes;
-    private BigDecimal value;
+    private List<TransactionBlueprints> transactionBlueprintsList;
     
 }
