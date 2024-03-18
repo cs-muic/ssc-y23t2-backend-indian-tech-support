e807c083f47881bdcd23db53b076ed82c2541bc0
Hir0Yat0 <xToppyThailandx@gmail.com>
2024-03-18 13:19:53 +0700

    feat: transaction blueprints service should be able to do get now

--- src/main/java/io/muzoo/ssc/project/backend/shortcuts/transactionblueprints/service/TransactionBlueprintsService.java
+++ src/main/java/io/muzoo/ssc/project/backend/shortcuts/transactionblueprints/service/TransactionBlueprintsService.java
@@ -1,39 +1,47 @@
 package io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import io.muzoo.ssc.project.backend.User.User;
 import io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.TransactionBlueprints;
 import io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.TransactionBlueprintsDTO;
 import io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.Type;
+import io.muzoo.ssc.project.backend.shortcuts.transactionblueprints.repositories.TransactionBlueprintsRepositories;
 import jakarta.servlet.http.HttpServletRequest;
 
 @Service
 public class TransactionBlueprintsService {
 
+    @Autowired
+    private TransactionBlueprintsRepositories transactionBlueprintsRepositories;
+
+    private void checkValidTypeAll(List<TransactionBlueprints> transactionBlueprintsList){
+        transactionBlueprintsList.forEach(
+            transactionBlueprints -> {
+                if (transactionBlueprints.getShortcutType().equals(Type.NONE)){
+                    throw new IllegalArgumentException("Invalid Transaction Blueprints Types!");
+                }
+            }
+        );
+    }
+
     public TransactionBlueprintsDTO getTransactionBlueprintsDTO(User user) {
-        final TransactionBlueprints transactionBlueprints = getTransactionBlueprints(user);
-        if (transactionBlueprints.getShortcutType().equals(Type.NONE)){
-            throw new IllegalArgumentException("Invalid Transaction Blueprints Types!");
-        }
+        final List<TransactionBlueprints> transactionBlueprintsList = getTransactionBlueprints(user);
+        checkValidTypeAll(transactionBlueprintsList); // suppose to throw if any one of them have wrong type NONE
         return TransactionBlueprintsDTO.builder()
-                .id(transactionBlueprints.getId())
-                .userId(transactionBlueprints.getUserId())
-                .tagId(transactionBlueprints.getTagId2())
-                .tagId2(transactionBlueprints.getTagId2())
-                .transactionType(transactionBlueprints.getTransactionType().name())
-                .shortcutType(transactionBlueprints.getShortcutType().getName())
-                .notes(transactionBlueprints.getNotes())
-                .value(transactionBlueprints.getValue())
-                .build();
+                   .transactionBlueprintsList(transactionBlueprintsList)
+                   .build(); 
     }
 
     public TransactionBlueprintsDTO postTransactionBlueprintsDTO(HttpServletRequest request, User user) {
         return null;
     }
     
-    public TransactionBlueprints getTransactionBlueprints(User user) {
-        return null;
+    public List<TransactionBlueprints> getTransactionBlueprints(User user) {
+        return transactionBlueprintsRepositories.findAllByUserId(user.getId());
     }
 
     public TransactionBlueprints postTransactionBlueprints(HttpServletRequest request, User user) {
