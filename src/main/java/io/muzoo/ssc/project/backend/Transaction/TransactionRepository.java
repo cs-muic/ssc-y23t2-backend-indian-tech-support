6ab1d5a2c85714958d020cd6fb5718ee2d72b30f
Nate-81 <nathans8100@gmail.com>
2024-03-21 23:08:45 +0700

    feat: correct queries for no tag chart implemented

--- src/main/java/io/muzoo/ssc/project/backend/Transaction/TransactionRepository.java
+++ src/main/java/io/muzoo/ssc/project/backend/Transaction/TransactionRepository.java
@@ -23,14 +23,16 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long>{
     @Query("SELECT t.tagId, SUM(t.value) FROM Transaction t WHERE t.userId = :userId AND MONTH(t.timestamp) = :month AND YEAR(t.timestamp) = 2018 AND t.type = :type GROUP BY t.tagId ORDER BY SUM(t.value) DESC")
     List<Object[]> sumAmountByUserIdAndMonthGroupByTag(long userId, int month, Type type);
 
-    @Query("SELECT SUM(t.value) FROM Transaction t WHERE t.userId = :userId AND t.timestamp BETWEEN :startDate AND :endDate " +
-            "AND t.type = :transactionType GROUP BY CASE :dateFormat WHEN 'Year' THEN YEAR(t.timestamp) WHEN 'Month' THEN DATE_FORMAT(t.timestamp, '%Y-%m')" +
+    @Query("SELECT CASE :dateFormat WHEN 'Day' THEN DATE_FORMAT(t.timestamp, '%Y-%m-%d') WHEN 'Month' THEN DATE_FORMAT(t.timestamp, '%Y-%m') " +
+            "WHEN 'Year' THEN DATE_FORMAT(t.timestamp, '%Y') END, SUM(t.value) FROM Transaction t WHERE t.userId = :userId AND t.timestamp BETWEEN :startDate AND :endDate " +
+            "AND t.type = :transactionType GROUP BY CASE :dateFormat WHEN 'Year' THEN DATE_FORMAT(t.timestamp, '%Y') WHEN 'Month' THEN DATE_FORMAT(t.timestamp, '%Y-%m')" +
             " WHEN 'Day' THEN DATE_FORMAT(t.timestamp, '%Y-%m-%d') END")
     List<Object[]> getChartDataNoTag(long userId, Timestamp startDate, Timestamp endDate, Type transactionType, String dateFormat);
 
-    @Query("SELECT SUM(t.value) FROM Transaction t WHERE t.userId = :userId AND t.timestamp BETWEEN :startDate AND :endDate " +
-            "AND t.type = :transactionType AND (t.tagId IN :tags OR t.tagId2 in :tags) GROUP BY CASE :dateFormat WHEN 'Year' THEN YEAR(t.timestamp) WHEN 'Month' THEN DATE_FORMAT(t.timestamp, '%Y-%m')" +
-            " WHEN 'Day' THEN DATE_FORMAT(t.timestamp, '%Y-%m-%d') END")
+    @Query("SELECT CASE :dateFormat WHEN 'Day' THEN DATE_FORMAT(t.timestamp, '%Y-%m-%d') WHEN 'Month' THEN DATE_FORMAT(t.timestamp, '%Y-%m') " +
+            "WHEN 'Year' THEN DATE_FORMAT(t.timestamp, '%Y') END, SUM(t.value) FROM Transaction t WHERE t.userId = :userId AND t.timestamp BETWEEN :startDate AND :endDate " +
+            "AND t.type = :transactionType AND (t.tagId IN :tags OR t.tagId2 in :tags) GROUP BY CASE :dateFormat WHEN 'Year' THEN DATE_FORMAT(t.timestamp, '%Y')" +
+            "WHEN 'Month' THEN DATE_FORMAT(t.timestamp, '%Y-%m') WHEN 'Day' THEN DATE_FORMAT(t.timestamp, '%Y-%m-%d') END")
     List<Object[]> getChartData(long userId, Timestamp startDate, Timestamp endDate, Type transactionType, String dateFormat, List<Long> tags);
 
 
