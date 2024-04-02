b779c7a660f0b4d4edab730aa3e27fabdaf72ff6
Anish Khanijaur <anishkhanijaur089@gmail.com>
2024-04-03 00:11:50 +0700

    feat: Deployment prep

--- start.sh
+++ start.sh
@@ -5,10 +5,18 @@ docker build -t api-service .
 
 # Run the Docker container
 docker run -d \
+  --restart unless-stopped \
   --env-file ./ignore/backend.env \
-  --name api_service \
+  --name api-service \
   -p 8081:8081 \
   --link maria_db:app_db \
   -v "$(pwd):/app" \
   -w /app \
   api-service
+
+  # export DOCKER_DEFAULT_PLATFORM=linux/arm64/v8
+# export DOCKER_DEFAULT_PLATFORM=linux/amd64/v3
+
+ # docker save api-service > backend.tar  
+    # scp -rp ./backend.tar anish@139.59.231.241:~/home
+    # docker load --input backend.tar
--- start_db.sh
+++ start_db.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# -e MYSQL_USER=root \
+# -e MYSQL_PASSWORD=root \
 
 docker run -d \
   --name maria_db \
   -p 13306:3306 \
+  -p 3306:3306 \
+  -p 172.17.0.1:13306:3306 \
   -e MYSQL_DATABASE=budget_webapp \
-  -e MYSQL_USER=root \
-  -e MYSQL_PASSWORD=root \
   -e MYSQL_ROOT_PASSWORD=root \
   mariadb:latest
